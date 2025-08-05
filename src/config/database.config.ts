904ed2c3ab2b9d9015981e80e6c93235b29295de
Phan Đình Trác <144455807+agomy1234@users.noreply.github.com>
2025-08-05 17:56:41 +0700

    update database.config.ts

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,5 +2,5 @@ import { registerAs } from '@nestjs/config';
 import * as process from 'process';
 
 export default registerAs('database', () => ({
-  uri: process.env.MONGODB_URI || 'mongodb://localhost/sportzone',
+  uri: process.env.MONGODB_URI || 'mongodb+srv://nhatnmde180:helloworld@cluster0.zufvinh.mongodb.net/SportZone?retryWrites=true&w=majority',
 }));
