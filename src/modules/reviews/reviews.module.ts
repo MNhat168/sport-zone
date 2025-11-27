916019742d69f8931b0c5083e4a71aa9d612d05f
Phan Đình Trác <144455807+agomy1234@users.noreply.github.com>
2025-11-27 20:20:36 +0700

    fix coach review to update the ratings and the total reviews

--- src/modules/reviews/reviews.module.ts
+++ src/modules/reviews/reviews.module.ts
@@ -4,12 +4,14 @@ import { ReviewsController } from './reviews.controller';
 import { ReviewsService } from './reviews.service';
 import { Review, ReviewSchema } from './entities/review.entity';
 import { Booking, BookingSchema } from '../bookings/entities/booking.entity';
+import { CoachProfile, CoachProfileSchema } from 'src/modules/coaches/entities/coach-profile.entity';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Review.name, schema: ReviewSchema },
       { name: Booking.name, schema: BookingSchema },
+      { name: CoachProfile.name, schema: CoachProfileSchema },
     ]),
   ],
   controllers: [ReviewsController],
